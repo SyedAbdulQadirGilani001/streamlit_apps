c43d62130cba99a7fb335ac86bd79e3387c85e9b
SyedAbdulQadirGilani001 <17pwcse1590@uetpeshawar.edu.pk>
2023-02-11 17:20:51 +0500

    added file

--- 01_plotly.py
+++ 01_plotly.py
@@ -1,9 +1,9 @@
-import streamlit as st
-from PIL import Image
-img = Image.open("st.png")
-st.image(img)
-video1 = open("Snow Leopards.mp4", "rb") 
-st.video(video1)
-#st.video(video1, start_time = 25)
-audio1 = open("audio.mp3", "rb")
-st.audio(audio1)
+from sklearn.ensemble import RandomForestClassifier 
+from explainerdashboard import ClassifierExplainer, ExplainerDashboard
+from explainerdashboard.datasets import titanic_survive, feature_descriptions
+X_train, y_train, X_test, y_test = titanic_survive()
+model=RandomForestClassifier().fit(X_train, y_train)
+explainer = ClassifierExplainer(model, X_test, y_test,cats=['Sex', 'Deck', 'Embarked'],
+                                descriptions=feature_descriptions,
+                                labels=['Not survived','Survived'])
+ExplainerDashboard(explainer).run()
